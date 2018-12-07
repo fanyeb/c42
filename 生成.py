275fba2a299904ed4e796432e4155829853cf04a
蓝落萧 <2320603602@qq.com>
2018-12-07 22:56:48 +0800

    2.4 定稿

--- 生成.py
+++ 生成.py
@@ -43,21 +43,21 @@ columns:
 ##w2 = [x for x in ww if len(x[1]) == 2]
 ##c2 = [x for x in cc if len(x[1]) == 2]
 ##c3 = [x for x in cc if len(x[1]) == 3]
-f = open('拆分表.txt', encoding = 'utf-8', mode = 'r')
+f = open('资料/拆分表.txt', encoding = 'utf-8', mode = 'r')
 l = [x.strip('\r\n').split('\t') for x in f]
 f.close()
-f = open('简码表.txt', encoding = 'utf-8', mode = 'r')
+f = open('资料/简码表.txt', encoding = 'utf-8', mode = 'r')
 b = [x.strip('\r\n').split('\t') for x in f]
 f.close()
-f = open('映射表.txt', encoding = 'utf-8', mode = 'r')
+f = open('资料/映射表.txt', encoding = 'utf-8', mode = 'r')
 d = {x.strip('\r\n').split('\t')[0]:x.strip('\r\n').split('\t')[1] for x in f}
 f.close()
-f = open('特码表.txt', encoding = 'utf-8', mode = 'r')
+f = open('资料/特码表.txt', encoding = 'utf-8', mode = 'r')
 t = [x.strip('\r\n').split('\t') for x in f]
 
 q = [(x[0], (d[x[1]]+d[x[2]]+d[x[3]]+x[4])[:3]) for x in l if x[5] == '0']
 qd = {x[0]:x[1] for x in q}
-f = open('c_42.dict.yaml', encoding = 'utf-8', mode = 'w')
+f = open('输出/c_42.dict.yaml', encoding = 'utf-8', mode = 'w')
 f.write(c_42_meta)
 for i in b:
     f.write(i[0] + '\t' + i[1] + '\n')
@@ -66,7 +66,7 @@ f.close()
 key = 'abcdefghijklmnopqrstuvwxyz;'
 key2 = key + ',./'
 a = [x + y + z for x in key for y in key2 for z in key]
-f = open('c_42a.dict.yaml', encoding = 'utf-8', mode = 'w')
+f = open('输出/c_42a.dict.yaml', encoding = 'utf-8', mode = 'w')
 f.write(c_42a_meta)
 c3 = []
 bc = [i[0] for i in b]
@@ -82,7 +82,7 @@ for i in t:
     f.write(i[0] + '\t' + i[1] + '\n')
 f.close()
 c = [(x[0], x[1]) for x in b if len(x[0]) == 1 and x[0] != '　']
-f = open('brevity.txt', encoding = 'utf-8', mode = 'w')
+f = open('输出/brevity.txt', encoding = 'utf-8', mode = 'w')
 for i in c:
     if i[1] != qd[i[0]][:2]:
         if i[1][-1] in ',./':
@@ -93,7 +93,7 @@ for i in c:
         f.write(i[0] + '\t' + i[1] + '\n')
 f.close()
 w = [(x[0], x[1]) for x in b if len(x[0]) == 2]
-f = open('brevity2.txt', encoding = 'utf-8', mode = 'w')
+f = open('输出/brevity2.txt', encoding = 'utf-8', mode = 'w')
 for i in w:
     f.write(i[0] + '\t' + i[1] + '\n')
 f.close()
