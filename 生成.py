11edd49a4d707a4083770071098db3312da45d21
蓝落萧 <2320603602@qq.com>
2018-12-08 20:57:26 +0800

    改一下文件结构

--- 生成.py
+++ 生成.py
@@ -10,6 +10,31 @@
 ##for x in i:
 ##    f.write(x + '\n')
 ##f.close()
+def strQ2B(ustring):
+    rstring = ""
+    for uchar in ustring:
+        inside_code=ord(uchar)
+        if inside_code == 12288:
+            inside_code = 32  
+        elif (inside_code >= 65281 and inside_code <= 65374): 
+            inside_code -= 65248
+ 
+        rstring += unichr(inside_code)
+    return rstring
+    
+def strB2Q(ustring):
+    rstring = ""
+    for uchar in ustring:
+        inside_code=ord(uchar)
+        if inside_code == 32:    
+            inside_code = 12288
+        elif inside_code >= 32 and inside_code <= 126:    
+            inside_code += 65248
+ 
+        rstring += chr(inside_code)
+    return rstring
+
+
 c_42_meta = '''# Rime dictionary: c_42
 # encoding: utf-8
 
@@ -52,10 +77,22 @@ f.close()
 f = open('资料/映射表.txt', encoding = 'utf-8', mode = 'r')
 d = {x.strip('\r\n').split('\t')[0]:x.strip('\r\n').split('\t')[1] for x in f}
 f.close()
+f = open('资料/映射表.txt', encoding = 'utf-8', mode = 'r')
+s = {x.strip('\r\n').split('\t')[0]:x.strip('\r\n').split('\t')[2] for x in f}
+for i in s:
+    if s[i] == '':
+        s[i] = i
+f.close()
 f = open('资料/特码表.txt', encoding = 'utf-8', mode = 'r')
 t = [x.strip('\r\n').split('\t') for x in f]
-
+f.close()
+u = [(x[0], (s[x[1]]+s[x[2]]+s[x[3]]+strB2Q(x[4].upper()))[:3]) for x in l if x[5] == '0']
 q = [(x[0], (d[x[1]]+d[x[2]]+d[x[3]]+x[4])[:3]) for x in l if x[5] == '0']
+qq = [(x[0], (d[x[1]]+d[x[2]]+d[x[3]]+x[4])[:3]) for x in l]
+f = open('输出/其他/全码表.txt', encoding = 'utf-8', mode = 'w')
+for i in q:
+    f.write(i[0] + '\t' + i[1] + '\n')
+f.close()
 qd = {x[0]:x[1] for x in q}
 f = open('输出/c_42.dict.yaml', encoding = 'utf-8', mode = 'w')
 f.write(c_42_meta)
@@ -64,8 +101,7 @@ for i in b:
     f.write('　' + '\t' + i[1] + '\n')
 f.close()
 key = 'abcdefghijklmnopqrstuvwxyz;'
-key2 = key + ',./'
-a = [x + y + z for x in key for y in key2 for z in key]
+a = [x + y + z for x in key for y in key for z in key]
 f = open('输出/c_42a.dict.yaml', encoding = 'utf-8', mode = 'w')
 f.write(c_42a_meta)
 c3 = []
@@ -82,18 +118,37 @@ for i in t:
     f.write(i[0] + '\t' + i[1] + '\n')
 f.close()
 c = [(x[0], x[1]) for x in b if len(x[0]) == 1 and x[0] != '　']
-f = open('输出/brevity.txt', encoding = 'utf-8', mode = 'w')
+a = [[],[],[]]
+f = open('输出/opencc/brevity.txt', encoding = 'utf-8', mode = 'w')
 for i in c:
     if i[1] != qd[i[0]][:2]:
         if i[1][-1] in ',./':
             f.write(i[0] + '\t~' + i[1] + '\n')
+            a[0].append(i)
         else:
             f.write(i[0] + '\t~~' + i[1] + '\n')
+            a[1].append(i)
     else:
         f.write(i[0] + '\t' + i[1] + '\n')
 f.close()
 w = [(x[0], x[1]) for x in b if len(x[0]) == 2]
-f = open('输出/brevity2.txt', encoding = 'utf-8', mode = 'w')
+f = open('输出/opencc/brevity2.txt', encoding = 'utf-8', mode = 'w')
 for i in w:
+    f.write(i[0] + '\t' + i[1] + '\n')
+    a[2].append(i)
+f.close()
+f = open('输出/opencc/division.txt', encoding = 'utf-8', mode = 'w')
+for i in u:
+    f.write(i[0] + '\t' + i[1] + '\n')
+f.close()
+f = open('输出/其他/选重、无理码和简词集锦.txt', encoding = 'utf-8', mode = 'w')
+f.write('选重：\n\n')
+for i in a[0]:
+    f.write(i[0] + '\t' + i[1] + '\n')
+f.write('\n无理：\n\n')
+for i in a[1]:
+    f.write(i[0] + '\t' + i[1] + '\n')
+f.write('\n简词：\n')
+for i in a[2]:
     f.write(i[0] + '\t' + i[1] + '\n')
 f.close()
